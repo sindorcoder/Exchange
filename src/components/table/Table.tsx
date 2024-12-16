fa16f3080ee9b0555a3676642495476af0acc050
@sindorcoder <sindordev77@gmail.com>
2024-12-16 15:45:08 +0500

    finish

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -11,9 +11,11 @@ const TableComponent = ({ data }: any) => {
   const [filterCategory, setFilterCategory] = useState("");
   const [filteredData, setFilteredData] = useState([]);
   const dispatch = useDispatch();
+
   const handleDelete = (data: any) => {
     dispatch(deleteTransaction(data));
   };
+
   useEffect(() => {
     let filteredData = [];
     let filteredCategory = [];
@@ -53,9 +55,9 @@ const TableComponent = ({ data }: any) => {
   };
 
   return (
-    <>
-      <header className="mb-[50px] flex items-center justify-between">
-        <div>
+    <div className="container mx-auto px-4">
+      <header className="mb-12 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+        <div className="flex gap-2">
           <Button onClick={() => setType("expence")} variant="">
             Expence
           </Button>
@@ -63,12 +65,16 @@ const TableComponent = ({ data }: any) => {
             Income
           </Button>
         </div>
-        <div className="flex items-center gap-2">
-          <span className="capitalize text-nowrap">Filtered by :</span>
+        <div className="block md:flex items-center gap-2">
+          <span className="capitalize text-nowrap text-sm text-gray-700">
+            Filtered by :
+          </span>
           <FormSelect
             defaultValue={filterDate}
             onChange={(e) => setFilterDate(e.target.value)}
+            className="max-w-[200px] my-4 md:m-0"
           >
+            <option value="">All Dates</option>
             {allDates.map((item: any, index: number) => (
               <option key={index} value={item}>
                 {item}
@@ -78,49 +84,57 @@ const TableComponent = ({ data }: any) => {
           <FormSelect
             defaultValue={filterCategory}
             onChange={(e) => setFilterCategory(e.target.value)}
+            className="max-w-[200px] my-4 md:m-0 "
           >
+            <option value="">All Categories</option>
             {category.map((item: any, index: number) => (
               <option key={index} value={item.name}>
                 {item.name}
               </option>
             ))}
           </FormSelect>
-          <Button onClick={handleResetInfo} className="!py-1 !text-[14px]">
+          <Button onClick={handleResetInfo} className="py-1 text-sm">
             Reset
           </Button>
         </div>
       </header>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Exchange</th>
-            <th>Category</th>
-            <th>Date</th>
-            <th>Amount</th>
-            <th>Comment</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((item: any) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.exchange}</td>
-              <td>{item.category}</td>
-              <td>{item.date}</td>
-              <td>{item.amount}</td>
-              <td>{item.comment}</td>
-              <td>
-                <Button onClick={() => handleDelete(item)} variant="Danger">
-                  Delete
-                </Button>
-              </td>
+      <div className="table-responsive">
+        <Table striped bordered hover className="text-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Exchange</th>
+              <th>Category</th>
+              <th>Date</th>
+              <th>Amount</th>
+              <th>Comment</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    </>
+          </thead>
+          <tbody>
+            {filteredData.map((item: any) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.exchange}</td>
+                <td>{item.category}</td>
+                <td>{item.date}</td>
+                <td>{item.amount}</td>
+                <td>{item.comment}</td>
+                <td>
+                  <Button
+                    onClick={() => handleDelete(item)}
+                    variant="danger"
+                    className="text-xs"
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
   );
 };
 
